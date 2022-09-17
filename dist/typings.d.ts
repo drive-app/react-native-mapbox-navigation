037843117bf4ee4bb004f87db43e404c8d6c8e42
Amrit Kahlon <amrit@hockey-community.com>
2022-09-17 01:11:51 -0700

    chore: typings

--- dist/typings.d.ts
+++ dist/typings.d.ts
@@ -3,35 +3,35 @@
  */
 declare type Coordinate = [number, number];
 declare type OnLocationChangeEvent = {
-  nativeEvent?: {
-    latitude: number;
-    longitude: number;
-  };
+    nativeEvent?: {
+        latitude: number;
+        longitude: number;
+    };
 };
 declare type OnRouteProgressChangeEvent = {
-  nativeEvent?: {
-    distanceTraveled: number;
-    durationRemaining: number;
-    fractionTraveled: number;
-    distanceRemaining: number;
-  };
+    nativeEvent?: {
+        distanceTraveled: number;
+        durationRemaining: number;
+        fractionTraveled: number;
+        distanceRemaining: number;
+    };
 };
 declare type OnErrorEvent = {
-  nativeEvent?: {
-    message?: string;
-  };
+    nativeEvent?: {
+        message?: string;
+    };
 };
 export interface IMapboxNavigationProps {
-  waypoints: Coordinate[];
-  shouldSimulateRoute?: boolean;
-  onLocationChange?: (event: OnLocationChangeEvent) => void;
-  onRouteProgressChange?: (event: OnRouteProgressChangeEvent) => void;
-  onError?: (event: OnErrorEvent) => void;
-  onCancelNavigation?: () => void;
-  onArrive?: () => void;
-  showsEndOfRouteFeedback?: boolean;
-  hideStatusView?: boolean;
-  mute?: boolean;
-  localeIdentifier?: string;
+    waypoints: Array<Coordinate>;
+    shouldSimulateRoute?: boolean;
+    onLocationChange?: (event: OnLocationChangeEvent) => void;
+    onRouteProgressChange?: (event: OnRouteProgressChangeEvent) => void;
+    onError?: (event: OnErrorEvent) => void;
+    onCancelNavigation?: () => void;
+    onArrive?: () => void;
+    showsEndOfRouteFeedback?: boolean;
+    hideStatusView?: boolean;
+    mute?: boolean;
+    localeIdentifier?: string;
 }
 export {};
